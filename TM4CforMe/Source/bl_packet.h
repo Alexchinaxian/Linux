6c5eff8a88b2c2f76f8492e479c36ef914968bf8
Alexchinaxian <1490434676@qq.com>
2022-05-29 10:48:54 +0800

    delete some

--- TM4CforMe/Source/bl_packet.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BL_PACKET_H__
-#define __BL_PACKET_H__
-#include <stdint.h>
-#include"tm4c1290nczad.h"
-extern int ReceivePacket(uint8_t *pui8Data, uint32_t *pui32Size);
-extern int SendPacket(uint8_t *pui8Data, uint32_t ui32Size);
-extern void AckPacket(void);
-
-#endif
